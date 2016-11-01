4d9a8dbeaf6f80d226ab1cb312186e8a1bf6977d
Freeze777 <freezefrancis@gmail.com>
2016-11-01 22:50:09 +0530

    refined repo

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,8 @@
 git add *
+git rm -r --cached *.bin
+git rm -r --cached *.out
+git rm -r --cached *.o
+git rm -r --cached *.*~
+git rm -r --cached *.zip
 git commit -a
-git push origin master
+git push https://github.com/Freeze777/graphix.git
